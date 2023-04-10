ce6d83fa54df9f9c60ce20234d52cdd9d8cf5b79
Arun Kumar <kumar05.962013@gmail.com>
2023-04-10 16:59:37 +0530

    baseUrl changes for goerli network

--- src/helpers/client.js
+++ src/helpers/client.js
@@ -1,17 +1,17 @@
 import { RequestNetwork } from "@requestnetwork/request-client.js";
 import { useCurrency } from "../contexts/CurrencyContext";
 export const useRequestClient = (network, signatureProvider) => {
-    const { currencyList } = useCurrency();
-    return getRequestClient(network, signatureProvider, currencyList);
+  const { currencyList } = useCurrency();
+  return getRequestClient(network, signatureProvider, currencyList);
 };
 export const getRequestClient = (network, signatureProvider, currencyList) => {
-    const requestNetwork = new RequestNetwork({
-        nodeConnectionConfig: {
-            baseURL: `https://${network}.gateway.request.network/`,
-        },
-        signatureProvider,
-        currencies: currencyList,
-    });
-    return requestNetwork;
+  const requestNetwork = new RequestNetwork({
+    nodeConnectionConfig: {
+      baseURL: network === "goerli" ? `https://${network}.gateway.request.network/` : `https://xdai.gateway.request.network/`,
+    },
+    signatureProvider,
+    currencies: currencyList,
+  });
+  return requestNetwork;
 };
 //# sourceMappingURL=client.js.map
